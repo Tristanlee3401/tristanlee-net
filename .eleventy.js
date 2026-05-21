1f9997f07d293a799841ecb0e14091a9e03e9aa2
tristanlee3401 <tristanlee3401123@gmail.com>
2026-05-21 16:52:25 +0800

    Update .eleventy.js with new directory

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   // Tell Eleventy to copy your assets straight through to the output folder
   eleventyConfig.addPassthroughCopy("src/styles.css");
   eleventyConfig.addPassthroughCopy("src/script.js");
+  eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/CNAME");
 
   return {
