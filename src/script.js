9c1fc34dfedf31620f0aea2b26af034dcb118a9e
tristanlee3401 <tristanlee3401123@gmail.com>
2026-05-21 12:51:19 +0800

    Rename /script.js filepath in index.html and remove redundant function from script.js

--- src/script.js
+++ src/script.js
@@ -22,11 +22,6 @@ function toggleTheme()
     }
 }
 
-function showMessage()
-{
-    alert("hello! thanks for clicking.");
-}
-
 function isValidEmail(email)
 {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -63,7 +58,6 @@ function submitForm()
     document.getElementById('email').value = '';
     document.getElementById('message').value = '';
 }
-
 
 function animation(elementId, text)
 {
