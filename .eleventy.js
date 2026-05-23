21a85e33e48d832301b9843aca3999433c39c7dd
tristanlee3401 <tristanlee3401123@gmail.com>
2026-05-23 20:59:01 +0800

    Refactor assets in a new structured directory tree and update _includes config

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Tell Eleventy to copy your assets straight through to the output folder
-  eleventyConfig.addPassthroughCopy("src/styles.css");
-  eleventyConfig.addPassthroughCopy("src/styles-2.css");
-  eleventyConfig.addPassthroughCopy("src/script.js");
-  eleventyConfig.addPassthroughCopy("src/images");
+  // eleventyConfig.addPassthroughCopy("src/assets/css/styles.css");
+  eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/CNAME");
 
   return {
