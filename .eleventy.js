4fbe38de0ece76326ca6e5f59c20bb884b281d01
tristanlee3401 <tristanlee3401123@gmail.com>
2026-05-24 15:59:00 +0800

    tells search engines how to crawl your site.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
   // Tell Eleventy to copy your assets straight through to the output folder
-  eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/CNAME");
+	eleventyConfig.addPassthroughCopy("src/assets");
+	eleventyConfig.addPassthroughCopy("src/CNAME");
+	eleventyConfig.addPassthroughCopy("src/robots.txt");
 
   return {
     dir: {
